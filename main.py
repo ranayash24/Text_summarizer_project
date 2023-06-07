54ae26f74d822c462043bfff70bbfe83d8562b68
ranayash24 <homehelp24022000@gmail.com>
2023-06-07 08:42:14 +0530

    data_ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from text_summarizer.pipeline.stage_01_data_ingesion import DataIngestionTrainingPipeline
+from text_summarizer.longging import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
